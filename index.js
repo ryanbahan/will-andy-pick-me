9c46904959072e58e409a7e75bcdbe6c9f20d33b
Ryan Bahan <ryan@ryanbahan.com>
2019-12-23 12:07:35 -0700

    Add iteration 2 functionality

--- index.js
+++ index.js
@@ -23,15 +23,20 @@ var answers = [
 
 var form = document.querySelector("form");
 var question = document.querySelector(".question");
+var clearBtn = document.querySelector(".clear-form");
 
 form.addEventListener('submit', generateResponse)
+form.addEventListener('input', checkFormValidity)
+clearBtn.addEventListener('click', clearForm)
 
 function generateResponse() {
   event.preventDefault();
   hideEightBall();
   insertQuestion(question.value);
   insertAnswer();
+  enableClearBtn();
   form.reset();
+  checkFormValidity();
 }
 
 function getRandomInt(max) {
@@ -44,11 +49,32 @@ function hideEightBall() {
 
 function insertQuestion(question) {
   var questionDisplay = document.querySelector(".question-display");
-  questionDisplay.innerHTML = `<div class="question-display">${question}</div>`;
+  questionDisplay.innerHTML = `<div class="question-display"><em>"${question}"</em></div>`;
 }
 
 function insertAnswer() {
   var randomInt = getRandomInt(answers.length);
   var answerDisplay = document.querySelector('.answer-display');
   answerDisplay.innerHTML = `<div class="answer-display">${answers[randomInt]}</div>`;
+}
+
+function checkFormValidity() {
+  if (form.checkValidity() === true) {
+    document.querySelector('.submit-btn').removeAttribute('disabled');
+  } else {
+    document.querySelector('.submit-btn').setAttribute('disabled', "");
+  }
+}
+
+function clearForm() {
+  document.querySelector('.eight-ball').style.display = "block";
+  document.querySelector('.question-display').innerHTML = "";
+  document.querySelector('.answer-display').innerHTML = "";
+  clearBtn.setAttribute('disabled', "");
+}
+
+function enableClearBtn() {
+  if (document.querySelector(".answer-display") != null) {
+    clearBtn.removeAttribute('disabled');
+  }
 }
